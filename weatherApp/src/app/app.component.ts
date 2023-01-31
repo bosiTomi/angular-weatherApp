64d039671c5674a76d8fdf759e23a08f66874a7c
Tamás Bosánszki <bosanszkitamas@gmail.com>
2023-01-31 22:02:54 +0100

    the ts code for showing dynamic data on the html

--- weatherApp/src/app/app.component.ts
+++ weatherApp/src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherData } from './models/weather.model';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'weatherApp';
+export class AppComponent implements OnInit {
+
+  cityName = 'Budapest';
+  weatherData: WeatherData = {} as WeatherData;
+  
+  constructor(private weatherService: WeatherService) { }
+  
+  ngOnInit(): void {
+    this.getWeatherData(this.cityName);
+  }
+  
+  onSubmit() {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+  
+  private getWeatherData(cityName: string) {
+    if (this.weatherData && this.weatherData.address === cityName) {
+      console.log(this.weatherData);
+    } else {
+      this.weatherService.getWeatherData(cityName)
+      .subscribe({
+        next: (response) => {
+          if (response) {
+            this.weatherData = response;
+            console.log(response);
+          }
+        }
+      });
+    }
+  }
+    
 }
